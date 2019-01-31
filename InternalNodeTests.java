adc5c00ad334e7eb444995b5bf7adbaf552d1401
Vincent Portelli <vinport16@gmail.com>
2019-01-31 17:10:30 -0500

    testing?

---
+++ InternalNodeTests.java
@@ -0,0 +1,34 @@
+package parser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+class InternalNodeTests {
+
+	@Test
+	void testInternalNodeFailsOnNull() {
+		assertThrows(NullPointerException.class, () -> {
+			InternalNode.build(null);
+		});
+	}
+
+	@Test
+	void testInternalNodesNotCached() {
+		List<Node> children = new LinkedList<>();
+		children.add(LeafNode.build(Variable.build("a")));
+		children.add(LeafNode.build(Connector.build(TerminalSymbol.TIMES)));
+		children.add(LeafNode.build(Variable.build("b")));
+		
+		assertFalse(InternalNode.build(children) == InternalNode.build(children));
+	}
+	
+}
--- Tests.java
+++ Tests.java
@@ -1,24 +1,18 @@
-import static org.junit.Assertions.assertEquals;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-import java.util.*;
 
-public class MyTests {
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
-    @Test
-    public void createExpressionTree() {
-        LeafNode c = LeafNode.build(Variable.build("c"));
-        LeafNode divide = LeafNode.build(Variable.build("/"));
-        InternalNode divideByC = InternalNode.build(Arrays.asList(c,divide));
 
-        LeafNode b = LeafNode.build(Variable.build("b"));
+public class Tests {
 
-        InternalNode bOverC = InternalNode.build(Arrays.asList(b,divideByC));
-
-        // assert statements
-        //assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
-        //assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
-        //assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+    public static void main(String[] args){
+      Result result = JUnitCore.runClasses(InternalNodeTest.class);
+      for (Failure failure : result.getFailures()) {
+        System.out.println(failure.toString());
+      }
     }
 }
